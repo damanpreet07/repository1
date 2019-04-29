a9c6e03db290c09f698f339ee1af518d68d9b88a
damanpreet07 <damanpreetkr97@example.com>
2019-04-29 13:08:58 +0530

    repository1

---
+++ idemo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class idemo
+{
+	public static void main(String arg[])
+	{
+	String name;
+	int rollno;
+	double marks;
+	long pin;
+	
+	System.out.println("Enter the name, rollno,marks and pin: ");
+	Scanner sc;
+	sc=new Scanner(System.in);
+	name=sc.next();
+	rollno=sc.nextInt();
+	marks=sc.nextDouble();
+	pin=sc.nextLong();
+	
+	System.out.println("Name is: "+name);
+	System.out.println("Name is: "+rollno);
+	System.out.println("Name is: "+marks);
+	System.out.println("Name is: "+pin);
+	}
+	}
